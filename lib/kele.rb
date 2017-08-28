fb9078bd3bf5feef53333bb3193ce33a6b79150c
Carissa Castro <carissabcastro@gmail.com>
2017-08-28 18:52:08 -0500

    Initial commit

---
+++ lib/kele.rb
@@ -0,0 +1,12 @@
+require 'httparty'
+
+class Kele
+  include HTTParty
+  base_uri "https://www.bloc.io/api/v1"
+
+  def initialize(email, password)
+    response = self.class.post('https://www.bloc.io/api/v1/sessions', body: {"email": email, "password": password})
+    raise "Invalid email/password" if response.code != 200
+    @auth_token = response["auth_token"]
+  end
+end
