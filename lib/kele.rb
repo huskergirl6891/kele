e8660d96b6b134004e2c644c5c632fff15bcdee3
Carissa Castro <carissabcastro@gmail.com>
2017-08-30 20:17:31 -0500

    Checkpoint 6 still not complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -21,11 +21,39 @@ class Kele
   def get_mentor_availability(mentor_id)
     response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
     JSON.parse response.body
+    # mentor id = 2362517
   end
 
-  def get_messages(*id)
-    response = self.class.get("https://www.bloc.io/api/v1/message_threads", values: { "page" => id }, headers: { "authorization" => @auth_token })
-    all_messages = JSON.parse response.body
-    result = all_messages["items"]
+  def get_messages(id=nil)
+    puts "id = "
+    puts id
+    if id != nil
+      response = self.class.get("https://www.bloc.io/api/v1/message_threads?page=1", headers: { "authorization" => @auth_token })
+      messages = JSON.parse response.body
+      result = messages["items"]
+    else
+      response = self.class.get("https://www.bloc.io/api/v1/message_threads?page=1", headers: { "authorization" => @auth_token })
+      messages = JSON.parse response.body
+      total_message_count = messages["count"]
+      result = messages["items"]
+      count = 1
+      total_message_count -= 10
+      while total_message_count > 0
+        puts "Total Message Count = "
+        puts total_message_count
+        count += 1
+        response = self.class.get("https://www.bloc.io/api/v1/message_threads?page=2", headers: { "authorization" => @auth_token })
+        temp_messages = JSON.parse response.body
+        result.concat temp_messages["items"]
+        total_message_count -= 10
+      end
+    end
+    result.each { |x| puts x }
+    puts
+  end
+
+  def create_messages(*token)
+    response = self.class.post('https://www.bloc.io/api/v1/messages', values: { "sender" => "carissabcastro@gmail.com", "recipient_id" => 2362517, "token" => token, "subject" => "Royale with cheese", "stripped-text" => "That's what they call a quarter pounder in France." }, headers: { "authorization" => @auth_token })
+    puts response
   end
 end
